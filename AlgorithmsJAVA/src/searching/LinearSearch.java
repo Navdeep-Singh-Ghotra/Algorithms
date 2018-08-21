752024cd84891253d1aeed21a9f8af1d8b64804f
Navdeep Singh <navdeepsingh.ghotra@gmail.com>
2018-08-21 19:22:12 -0400

    Added driver

--- AlgorithmsJAVA/src/searching/LinearSearch.java
+++ AlgorithmsJAVA/src/searching/LinearSearch.java
@@ -2,5 +2,16 @@ package searching;
 
 public class LinearSearch {
 	//logic for linear search 
+	public int search(int arr[], int x)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+  
+        // If element not found, Return -1
+        return -1;
+    }
 
 }
